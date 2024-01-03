104f5c47b54516bc2927e8fdc7611e0b9074a88f
Mohammad Faisal Nazari <mfaisalnaari@gmail.com>
2024-01-03 12:02:21 -0700

    first part complete

--- lib/ship.rb
+++ lib/ship.rb
@@ -3,13 +3,17 @@ class Ship
               :length,
               :health
 
-  def initialize
+  def initialize (name,length)
     @name = name
     @length = length
     @health = length
   end
 
   def sunk?
-
+    health <= 0
+  end
+  
+  def hit
+    @health -= 1
   end
 end
