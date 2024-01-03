6dc14929ff1c0c0ce5dc2d8acfcbd29e62a0aa93
Hoa Dam <hoangocdam@gmail.com>
2024-01-03 15:27:44 -0800

    feat: valid placement and valid coordinate

--- lib/board.rb
+++ lib/board.rb
@@ -18,4 +18,35 @@ class Board
 
     cells
   end
+
+  def valid_coordinate?(coordinate)
+    @cells.key?(coordinate)
+  end
+
+  def valid_placement?(ship, coordinates)
+    return false if coordinates.length != ship.length
+    columns = coordinates.map {|coordinate| coordinate[0]}
+    rows = coordinates.map {|coordinate| coordinate[1].to_i}
+    return true if consecutive_columns?(columns,rows,ship.length) || consecutive_rows?(columns, rows,ship.length)
+
+    false
+  end
+
+  def same_row?(rows)
+    rows.uniq.length == 1
+  end
+
+  def same_column?(columns)
+    columns.uniq.length == 1
+  end
+
+  def consecutive_columns?(columns,rows,length)
+    last_column = (columns[0].ord + (length-1)).chr
+    same_row?(rows) && columns == (columns[0]..last_column).to_a
+  end
+
+  def consecutive_rows?(columns, rows,length)
+    last_row = rows[0]+ (length-1)
+    same_column?(columns) && rows == (rows[0]..last_row).to_a
+  end
 end
