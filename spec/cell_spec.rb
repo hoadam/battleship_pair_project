5360034fb9f3ef5d31a08ca2253d46801215b438
Hoa Dam <hoangocdam@gmail.com>
2024-01-03 12:17:29 -0800

    test:place_ship

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,7 +2,7 @@ require './spec/spec_helper'
 
 RSpec.describe Cell do
   let(:cell_1) { Cell.new("B4") }
-
+  let(:cruiser) { Ship.new("Cruiser", 3) }
   describe '#initialize' do
     it 'can initialize' do
       expect(cell.coordinate).to eq("B4")
@@ -10,5 +10,17 @@ RSpec.describe Cell do
     end
   end
 
+  describe '#empty?' do
+    it 'returns true if the cell has no ship' do
+      expect(cell.empty?).to eq(true)
+    end
+  end
 
+  describe '#place_ship' do
+    it 'places ship on the cells' do
+      cell.place_ship(cruiser)
+      expect(cell.ship).to eq(cruiser)
+      expect(cell.empty?).to eq(false)
+    end
+  end
 end
