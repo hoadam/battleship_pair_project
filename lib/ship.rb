6758b60439a06ac8d106e5727ef65c31b9512774
Hoa Dam <hoangocdam@gmail.com>
2024-01-03 10:50:50 -0800

    Initial commit

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,3 +1,15 @@
 class Ship
+  attr_reader :name,
+              :length,
+              :health
 
+  def initialize
+    @name = name
+    @length = length
+    @health = length
+  end
+
+  def sunk?
+
+  end
 end
