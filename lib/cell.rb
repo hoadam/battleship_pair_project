42f9c5fa94eea56d979fe7b3b30159e36990309d
Hoa Dam <hoangocdam@gmail.com>
2024-01-03 13:04:55 -0800

    feat:render

--- lib/cell.rb
+++ lib/cell.rb
@@ -25,5 +25,11 @@ class Cell
       @ship.hit if @ship != nil
     end
 
-
+    def render(reveal_ship = false)
+      return "S" if reveal_ship == true && @ship != nil
+      return "." if fired_upon == false
+      return "M" if fired_upon == true && @ship == nil
+      return "X" if fired_upon == true && @ship.sunk?
+      return "H" if fired_upon == true && @ship != nil
+    end
 end
