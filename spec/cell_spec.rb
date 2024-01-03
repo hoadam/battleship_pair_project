40f1e4a2e1b9e5db227466abfbddac5ffb15b765
Hoa Dam <hoangocdam@gmail.com>
2024-01-03 12:12:31 -0800

    test: initialize

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,1 +1,14 @@
 require './spec/spec_helper'
+
+RSpec.describe Cell do
+  let(:cell_1) { Cell.new("B4") }
+
+  describe '#initialize' do
+    it 'can initialize' do
+      expect(cell.coordinate).to eq("B4")
+      expect(cell.ship).to be_nil
+    end
+  end
+
+
+end
