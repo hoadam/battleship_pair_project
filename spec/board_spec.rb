7e33e7a282faed5106d65f694dc9df5707c85064
Mohammad Faisal Nazari <mfaisalnaari@gmail.com>
2024-01-03 21:45:12 -0700

    iteration 2 tests complete

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -49,7 +49,7 @@ RSpec.describe Board do
         end
     end
 
-    describe 'placing ship' do
+    describe 'place' do
         it 'places ship on cells in a board' do
             board.place(cruiser, ["A1","A2","A3"])
             cell_1 = board.cells["A1"]   
@@ -63,6 +63,14 @@ RSpec.describe Board do
         it 'ships cannot overlap' do
             board.place(cruiser, ["A1","A2","A3"])
             expect(board.valid_placement?(submarine, ["A1", "B1"])).to eq(false)
+        end
+    end
+
+    describe 'render' do
+        it 'render a string of representation of the board' do
+            board.place(cruiser, ["A1","A2","A3"])
+            expect(board.render).to eq("  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n")
+            expect(board.render(true)).to eq("  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n")
         end
     end
 end
